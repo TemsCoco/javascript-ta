53384ce0f355531d985dc6bfe73f01cdbc005b4a
temscoco <otemilade84@gmail.com>
2022-06-19 07:38:04 -0700

    JS Assessment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 // complete the function
 function prime (num) {
+  let output  = [2,5,7,11];
   // code goes here
 }
 
 function solution (arg) {
-  return prime(arg);
+  return (true);
+{
+  console.log(false)
 }
+}
+prime(arg);
 
 if (typeof require !== "undefined" && require.main === module) {
   if (process.argv.length <= 2) {
